077c44322b630690ea5e7a9c8736a3ead5d74248
ColeVoelpel <66435041+ColeVoelpel@users.noreply.github.com>
2021-03-08 14:02:47 -0600

    Added dockerfile, renamed html file

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 async function submitTag() {
   //Getting the tag input to search on
   const tag = document.getElementById("tag").value;
+
   //Getting last weeks date
   var lastWeek = new Date();
   var pastDate = lastWeek.getDate() - 7;
   lastWeek.setDate(pastDate);
   lastWeek = Math.round(lastWeek.getTime() / 1000);
-  console.log(lastWeek);
+
   //Doing API call to stack overflow
+  const sendTime = new Date().getTime();
   const recentQuestion = `https://api.stackexchange.com/2.2/questions?page=1&pagesize=10&fromdate=${lastWeek}&order=desc&sort=creation&tagged=${tag}&site=stackoverflow&filter=!*PBR8nclYi)J1Y.yEkh5DVscPK_BbQgKnuB)FPIQtKjQxarST`;
   let recentResponse = await fetch(recentQuestion);
-  let recentJSON = await recentResponse.json();
-  let recentItems = recentJSON.items;
   const hotQuestion = `https://api.stackexchange.com/2.2/questions?page=1&pagesize=10&fromdate=${lastWeek}&order=desc&sort=votes&tagged=${tag}&site=stackoverflow&filter=!*PBR8nclYi)J1Y.yEkh5DVscPK_BbQgKnuB)FPIQtKjQxarST`;
   let response = await fetch(hotQuestion);
+  const receiveTime = new Date().getTime();
+  const responseTime = receiveTime - sendTime;
+
+  //Getting the JSON from the responses
+  let recentJSON = await recentResponse.json();
+  let recentItems = recentJSON.items;
+
   let voteResponse = await response.json();
   let voteItems = voteResponse.items;
+
+  //Concatinating the lists together and sorting them
   var finalObj = voteItems.concat(recentItems);
   finalObj.sort(function (a, b) {
     return b.creation_date - a.creation_date;
@@ -33,7 +42,7 @@ async function submitTag() {
     buttondiv.innerHTML +=
       `<button type="button" onclick="collapse(${i})" class="btn btn-outline-secondary btn-lg col-12 collapsible">${
         finalObj[i].title
-      } || ${date} || ${finalObj[i].score} </button>
+      } || ${date} || Score: ${finalObj[i].score} </button>
     <div id="${i}" style="display: none" class="content">
     <h2>Question</h2>
     <div class="card">
@@ -51,6 +60,9 @@ async function submitTag() {
     //Appending the div onto the div container
     formdiv.appendChild(buttondiv);
   }
+  var responseDiv = document.createElement("div");
+  responseDiv.innerHTML += `<p>The response time is ${responseTime} ms'. </p>`;
+  formdiv.appendChild(responseDiv);
 }
 
 function appendComments(items) {
@@ -77,9 +89,9 @@ function appendAnswers(items) {
     for (var j = 0; j < items.answers.length; j++) {
       answers += `<div class="card">
         <div class="card-header">
-          Answer ${j} Score: ${items.answers[j].score} Created: ${epochToDate(
-        items.answers[j].creation_date
-      )}
+          <b>Answer ${j}</b> Score: ${
+        items.answers[j].score
+      } Created: ${epochToDate(items.answers[j].creation_date)}
         </div>
         <div class="card-body">
           ${items.answers[j].body}
